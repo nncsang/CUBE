9f39724b0ebc20737533a01da79f6990a535719f
Sang Nguyen <nncsang@nncsang.local>
2014-12-21 19:27:57 +0100

    create data for rnds

---
+++ src/com/hadoop/cube/CreateDataBin3.java
@@ -0,0 +1,104 @@
+package com.hadoop.cube;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.SequenceFile;
+
+import com.hadoop.cube.TimeStampWritable;;
+
+public class CreateDataBin3 {
+
+	public static void main(String[] args) throws IOException {
+		
+		if (args.length != 2 && args.length != 3){
+			System.out.println("Usage: <input> <output>");
+			return;
+		}
+		String[] input = {args[0]};
+		String output = args[1];
+		int limit;
+		try{
+			limit = Integer.valueOf(args[2]);
+		}catch(Exception ex){
+			limit = -1;
+		}
+		// TODO Auto-generated method stub
+		
+		Configuration conf = new Configuration();
+		
+		FileSystem fs = FileSystem.get(conf);
+		Path outFile = new Path(output);
+		SequenceFile.Writer writer = null;
+		
+		
+		
+		String strLine ="";
+		long current = 0;
+		TimeStampWritable key = new TimeStampWritable();
+		LongWritable value = new LongWritable();
+        writer = SequenceFile.createWriter(fs, conf, outFile, key.getClass(), value.getClass());
+        
+        for (int i = 0; i < input.length; i++){
+        	System.out.println("-------------------------------------------");
+        	System.out.println("Starting to write data of " + input[i]);
+        	System.out.println("");
+	        FileInputStream fstream = new FileInputStream(input[i]);
+			BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+			
+			while ((strLine = br.readLine()) != null)   {
+				String[] attributes = strLine.split("\t");
+				
+		        try{
+		        	int year = Integer.parseInt(attributes[0]);
+			        int month = Integer.parseInt(attributes[1]);
+			        int dayOfMonth = Integer.parseInt(attributes[2]);
+			        
+			        int flightNumber = Integer.parseInt(attributes[3]);
+			        int origin = Integer.parseInt(attributes[4]);
+			        int p = Integer.parseInt(attributes[5]);
+			        
+			        
+			        key.set(year, month, dayOfMonth, flightNumber, origin);
+		            value.set(p);
+		            writer.append(key, value);
+		            
+		            current++;
+		            if (current % 1000000 == 0){
+		            	System.out.println("Num of written tuples: " + current);
+		            }
+		            
+		            if (current == limit && limit != -1)
+		            	break;
+		            	
+		        }
+		        catch(Exception ex){
+		        	
+		        }
+			}
+			
+			br.close();
+			fstream.close();
+				
+			if (current == limit && limit != -1)
+            	break;
+	    }
+        
+		
+		IOUtils.closeStream(writer);
+		
+		System.out.println("");
+		System.out.println("Total written tuples: " + current);
+		System.out.println("Done");
+	}
+
+}
---
+++ src/com/hadoop/cube/TimeStampWritable.java
@@ -0,0 +1,107 @@
+package com.hadoop.cube;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
+
+public class TimeStampWritable implements WritableComparable<TimeStampWritable> {
+    
+    public static int length = 5;
+    public static int NullValue = -1;
+    public int[] fields = new int[length];
+        
+    public TimeStampWritable() {    
+    }
+    
+    public TimeStampWritable(int[] f) {
+        fields = f;
+    }
+    
+    public TimeStampWritable(int year, int month, int dayOfMonth, int minute, int second) {
+        fields[0] = year;
+        fields[1] = month;
+        fields[2] = dayOfMonth;
+        fields[3] = minute;
+        fields[4] = second;
+    }
+    
+    public void set(int[] f) {
+        fields = f;
+    }
+    
+    public void set(int year, int month, int dayOfMonth, int minute, int second) {
+        fields[0] = year;
+        fields[1] = month;
+        fields[2] = dayOfMonth;
+        fields[3] = minute;
+        fields[4] = second;
+    }
+
+    @Override
+    public void readFields(DataInput in) throws IOException {
+        for (int i = 0; i < length; i++)
+            fields[i] = in.readInt();
+    }
+
+    @Override
+    public void write(DataOutput out) throws IOException {
+        for (int i = 0; i < length; i++)
+            out.writeInt(fields[i]);
+    }
+
+    @Override
+    public int compareTo(TimeStampWritable obj) {
+    	if (obj == null)
+    		return -1;
+    	
+    	if (obj.fields == null)
+    		return -1;
+    	
+        for (int i = 0; i < length; i++) {
+            if (fields[i] < obj.fields[i])
+                return -1;
+            else if (fields[i] > obj.fields[i])
+                return 1;
+        }
+        return 0;
+        
+    }
+    
+    public String toString() {
+        if (fields == null)
+            return "";
+        String t = String.valueOf(fields[0]); 
+        for (int i = 1; i < length; i++) {
+            t += "\t" + String.valueOf(fields[i]);
+        }
+        return t; 
+    }
+    
+    /** A Comparator optimized for IntWritable. */ 
+    public static class Comparator extends WritableComparator {
+      public Comparator() {
+        super(TimeStampWritable.class);
+      }
+
+      public int compare(byte[] b1, int s1, int l1,
+                         byte[] b2, int s2, int l2) {
+          int i1, i2;
+          for (int i = 0; i < TimeStampWritable.length; i++) {
+              i1 = readInt(b1, s1+ i*4);
+              i2 = readInt(b2, s2+ i*4);
+              
+              if (i1 < i2) return -1;
+              else if (i1 > i2) return 1;
+          }
+           
+          return 0;
+      }
+    }
+
+    static {                                        // register this comparator
+      WritableComparator.define(TimeStampWritable.class, new Comparator());
+    }
+}
