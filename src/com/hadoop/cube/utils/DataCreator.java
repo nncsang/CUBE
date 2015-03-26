3d701ce0b0bb24b092c7566d0abe5eb131819319
NGUYEN Ngoc Chau Sang <ngoc-chau-sang.nguyen@eurecom.fr>
2015-03-26 20:46:15 +0100

    update jar

--- src/com/hadoop/cube/utils/DataCreator.java
+++ src/com/hadoop/cube/utils/DataCreator.java
@@ -23,7 +23,7 @@ public class DataCreator {
 		Random random = new Random();
 		long dataSize = Long.parseLong(args[1]) * 1024 * 1024 * 1024;
 		long noYear = Integer.parseInt(args[2]);
-		long oneGB = 1024 * 1024;
+		long oneGB = 1024 * 1024 * 1024;
 		String writeData = "";
 		long cnt = 0;
 		while (true) {
